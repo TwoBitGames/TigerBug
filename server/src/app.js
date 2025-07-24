5ffcf3d450d208fc25363c247b4f06de4a42ab78
Mathias Wagner <germannewsmaker@gmail.com>
2025-07-24 16:44:19 +0200

    Update rate limiting

--- server/src/app.js
+++ server/src/app.js
@@ -25,7 +25,9 @@ app.use(helmet({
 }));
 
 const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000,   max: 100,   message: 'Too many requests from this IP, please try again later.',
+  windowMs: 15 * 60 * 1000,
+  max: 1000,
+  message: 'Too many requests from this IP, please try again later.',
 });
 app.use(limiter);
 
